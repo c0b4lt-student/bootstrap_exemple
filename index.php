4e19b73d6ab59b32724e37bce758839805162738
c0b4lt-student <student.corentin.turgis@gmail.com>
2021-10-07 14:33:17 +0200

    New font 'Hardball' added; navbar and header functional

--- index.php
+++ index.php
@@ -1,38 +1,92 @@
 <!DOCTYPE html>
-<html lang="'fr">
+<html lang="fr" xmlns="http://www.w3.org/1999/html">
   <head>
     <meta charset="utf-8" />
-    <title>My page title</title>
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-      <link rel="stylesheet" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no  " />
+    <title>Amnesia</title>
+    <link rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css"
+          integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU"
+          crossorigin="anonymous" />
+    <link rel="stylesheet"
+          href="style.css" />
   </head>
 
   <body>
-    <header>
-      <h1>Header</h1>
+  <div class="container-fluid">
+<!--Header-->
+    <header class="row justify-content-between shadow-lg">
+      <div class="logo col-3">
+        <p class="row h-100 justify-content-around align-items-center">LOGO</p>
+      </div>
+      <div class="col-6">
+        <p class="row justify-content-around align-items-center title">Amnesia</p>
+      </div>
+      <div class="badge col-3">
+        <p class="row h-100 justify-content-center align-items-center">BADGE</p>
+      </div>
     </header>
-
-    <nav class="navbar navbar-expand-lg">
-      <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Our team</a></li>
-        <li><a href="#">Projects</a></li>
-        <li><a href="#">Contact</a></li>
-      </ul>
+<!--Navbar collapsed-->
+<div class="position-fixed top-0 end-0">
+  <div class="my_navbar collapse navbar flex-column rounded-bottom" id="navbarToggleExternalContent">
+    <a class="navbar-brand my_nav-text-color" href="#">Amnesia</a>
+    <ul class="nav flex-column nav-pills">
+      <li class="nav-item">
+        <a class="nav-link active my_nav-text-color" href="#">Qui sommes-nous ?</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link my_nav-text-color" href="#">Nos prochains concerts</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link my_nav-text-color" href="#">Nous contacter</a>
+      </li>
+    </ul>
+  </div>
+    <nav class="position-fixed top-0 end-0 navbar rounded-circle">
+      <button class="navbar-toggler shadow-none" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+          </svg>
+        </span>
+      </button>
     </nav>
+</div>
 
-    <main>
-      <article>
-        <h2>Article heading</h2>
-      </article>
-
-      <aside>
-        <h2>Related</h2>
-      </aside>
+    <main class="row m-3">
+      <div class="col-12">
+        <div class="row justify-content-xl-around">
+          <article class="article1 col-12 col-md-4 col-xl-3">
+            <p class="row h-100 justify-content-center align-items-center">ARTICLE 1</p>
+          </article>
+          <article class="article2 col-12 col-md-4 col-xl-3">
+            <p class="row h-100 justify-content-center align-items-center">ARTICLE 2</p>
+          </article>
+          <article class="article3 col-12 col-md-4 col-xl-3">
+            <p class="row h-100 justify-content-center align-items-center">ARTICLE 3</p>
+          </article>
+        </div>
+        <article class="article4 row">
+          <p class="row h-100 justify-content-center align-items-center">ARTICLE 4</p>
+        </article>
+      </div>
     </main>
 
-    <footer>
-      <p>©Copyright 2050 by nobody. All rights reversed.</p>
+    <footer class="row">
+      <p class="row h-100 justify-content-center align-items-center">FOOTER</p>
     </footer>
+  </div>
+
+
+    <script
+            src="https://code.jquery.com/jquery-3.6.0.min.js"
+            integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+            crossorigin="anonymous">
+    </script>
+    <script
+            src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+            integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+            crossorigin="anonymous">
+    </script>
   </body>
 </html>
